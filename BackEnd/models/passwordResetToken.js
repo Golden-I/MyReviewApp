29ef0935d807429618496cdcf45d208af1ac4b2a
Golden-I <65423579+Golden-I@users.noreply.github.com>
2023-04-20 22:46:09 +0200

    PaswordResetModels

---
+++ BackEnd/models/passwordResetToken.js
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+
+const passwordRestTokenSchema = mongoose.Schema({
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "user",
+    required: true,
+  },
+  token: {
+    type: String,
+    required: true,
+  },
+  createAt: {
+    type: Date,
+    expires: 3600, //60 min x 60 second (after one hour expire)
+    default: Date.now(),
+  },
+});
+
+passwordRestTokenSchema.pre("save", async (next) => {
+  if (this.isModified("token")) {
+    this.token = await bcrypt.hash(this.token, 10);
+  }
+  next();
+});
+passwordRestTokenSchema.methods.compareToken = async function (token) {
+  const result = await bcrypt.compare(token, this.token);
+  return result;
+};
+
+module.exports = mongoose.model("passwordRestToken", passwordRestTokenSchema);
