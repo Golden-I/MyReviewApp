eefbfe2c2cfd8e29b7e11b71220d1e79016e2095
Golden-I <65423579+Golden-I@users.noreply.github.com>
2023-04-20 23:45:35 +0200

    ResetPasswordUrl

--- BackEnd/controller/user.js
+++ BackEnd/controller/user.js
@@ -1,8 +1,9 @@
-const nodemailer = require("nodemailer");
+const crypto = require("crypto");
 const User = require("../models/user.js");
 const {
   EmailVerificationToken,
 } = require("../models/emailVerificationToken.js");
+const { passwordResetToken } = require("../models/passwordResetToken.js");
 const { generateOTP, generateMailTransporter } = require("../utils/mail.js");
 const { isValidObjectId } = require("mongoose");
 const { sendError } = require("../utils/helper.js");
@@ -121,4 +122,40 @@ exports.resendEmailVerificationToken = async (req, res) => {
   res.status(200).json({
     message: "New OTP has been sent to your registered email account.",
   });
+};
+
+exports.forgetPassword = async (req, rest) => {
+  const { email } = req.body;
+  if (!email) return sendError(res, "Email is Missing!");
+  const user = await user.findOne({ email });
+  if (!user) return sendError(res, "user not found!", 404);
+
+  const alreadyHasToken = await passwordResetToken.findOne({ owner: user._id });
+  if (alreadyHasToken)
+    return sendError(
+      res,
+      "only after one hour you can request for another token!"
+    );
+  const token = await generateRandomByte();
+  const newPasswordResetToken = await PasswordResetToken({
+    owner: user._id,
+    token,
+  });
+  await newPasswordResetToken.save();
+
+  const resetPasswordUrl = `http://localhost:3000/reset-password?token=${token}&id=${user._id}`;
+
+  const transport = generateMailTransporter();
+
+  transport.sendMail({
+    from: "security@reviewapp.com",
+    to: user.email,
+    subject: "Reset Password Link",
+    html: `
+        <p>Click here to reset password</p>
+        <a href='${resetPasswordUrl}'>Change Password</a>
+      `,
+  });
+
+  res.json({ message: "Link sent to your email!" });
 };
--- BackEnd/routes/user.js
+++ BackEnd/routes/user.js
@@ -1,12 +1,19 @@
 const express = require("express");
 // const { check } = require("express-validator");
-const { create, verifyEmail } = require("../controller/user");
+const {
+  create,
+  verifyEmail,
+  resendEmailVerificationToken,
+  forgetPassword,
+} = require("../controller/user");
 const { userValidator, validate } = require("../middleWares/validator.js");
 
 const router = express.Router();
 
 router.post("/create", userValidator, validate, create);
 router.post("/verify-email", verifyEmail);
+router.post("/resend-email-verification-token", resendEmailVerificationToken);
+router.post("/forget-password", forgetPassword);
 
 module.exports = router;
 
