7ad61ec29c61e97fc7684abc95441d94f7e838e4
Golden-I <65423579+Golden-I@users.noreply.github.com>
2023-05-12 23:21:44 +0300

    front end

---
+++ Frontend/src/app.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Navbar from "../src/components/user/Navbar";
+import Signin from "../src/components/auth/Signin";
+
+export default function App() {
+  return (
+    <>
+      <Navbar />
+      <Signin />
+    </>
+  );
+}
